cac93b24f36cd980d17f73e16b82c30c27b5ffff
Alin Stanciu <alin1stanciu@gmail.com>
2023-01-23 18:33:25 +0200

    Added User entity

---
+++ src/main/java/alin/licenta/be/entities/User.java
@@ -0,0 +1,34 @@
+package alin.licenta.be.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private String password;
+
+    private String defaultDeliveryAddress;
+
+    private String defaultBillingAddress;
+
+    @OneToMany(mappedBy = "user")
+    private List<Cart> carts;
+}
