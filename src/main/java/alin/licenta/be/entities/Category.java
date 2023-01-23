ca825bac7db7cc7ff12fa97899fdd68d17f67967
Alin Stanciu <alin1stanciu@gmail.com>
2023-01-23 18:32:57 +0200

    Added Category entity

---
+++ src/main/java/alin/licenta/be/entities/Category.java
@@ -0,0 +1,22 @@
+package alin.licenta.be.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Subcategory> subcategoryList;
+}
