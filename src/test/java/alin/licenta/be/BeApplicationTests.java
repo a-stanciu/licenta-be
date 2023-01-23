9f195a5a060bf2855b93cbbc83babfb1d625f6b2
Alin Stanciu <alin1stanciu@gmail.com>
2023-01-23 13:55:37 +0200

    Initial commit

---
+++ src/test/java/alin/licenta/be/BeApplicationTests.java
@@ -0,0 +1,13 @@
+package alin.licenta.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
