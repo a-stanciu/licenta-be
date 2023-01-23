4e3e92c734fc5e920e1d3dde7b8bf76829e82a23
Alin Stanciu <alin1stanciu@gmail.com>
2023-01-23 18:33:33 +0200

    Added Value entity

---
+++ src/main/java/alin/licenta/be/entities/Value.java
@@ -0,0 +1,22 @@
+package alin.licenta.be.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Value {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String value;
+
+    @OneToMany(mappedBy = "value")
+    private List<AttributeValue> AttributeValueList;
+}
