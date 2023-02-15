63abd8c69121c058cf1eb96d972eedc62330741d
Alin Stanciu <alin1stanciu@gmail.com>
2023-02-15 14:07:29 +0200

    Add custom methods to CartRepository

--- src/main/java/alin/licenta/be/repositories/CartRepository.java
+++ src/main/java/alin/licenta/be/repositories/CartRepository.java
@@ -3,5 +3,12 @@ package alin.licenta.be.repositories;
 import alin.licenta.be.entities.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CartRepository extends JpaRepository<Cart, Integer> {
+
+    List<Cart> findByUser(int userId);
+
+
+    Cart findCurrentByUser(int userId);
 }
