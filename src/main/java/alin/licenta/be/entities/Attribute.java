4348fab5f832bbe87bed1389afbf836e620f4c76
Alin Stanciu <alin1stanciu@gmail.com>
2023-05-17 17:25:39 +0300

    Update imports

--- src/main/java/alin/licenta/be/entities/Attribute.java
+++ src/main/java/alin/licenta/be/entities/Attribute.java
@@ -1,7 +1,7 @@
 package alin.licenta.be.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
+import javax.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
--- src/main/java/alin/licenta/be/entities/Category.java
+++ src/main/java/alin/licenta/be/entities/Category.java
@@ -1,6 +1,6 @@
 package alin.licenta.be.entities;
 
-import jakarta.persistence.*;
+import javax.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
