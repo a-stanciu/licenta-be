fa33e54a58306aef2f258ff94c56a8f8d192adb5
Alin Stanciu <alin1stanciu@gmail.com>
2023-05-17 17:27:51 +0300

    Update cart repository

--- src/main/java/alin/licenta/be/repositories/CartRepository.java
+++ src/main/java/alin/licenta/be/repositories/CartRepository.java
@@ -1,14 +1,14 @@
 package alin.licenta.be.repositories;
 
 import alin.licenta.be.entities.Cart;
+import alin.licenta.be.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CartRepository extends JpaRepository<Cart, Integer> {
 
-    List<Cart> findByUser(int userId);
+    List<Cart> findCartsByUser(User user);
 
-
-    Cart findCurrentByUser(int userId);
+    Cart findTop1CartByUserOrderByLastModifiedDesc(User user);
 }
