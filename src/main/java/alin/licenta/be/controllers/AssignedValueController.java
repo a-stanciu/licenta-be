36c465a99d9ac9749404753d9c942bef07ff97db
Alin Stanciu <alin1stanciu@gmail.com>
2023-06-07 12:08:49 +0300

    Create controller for AssignedValue

---
+++ src/main/java/alin/licenta/be/controllers/AssignedValueController.java
@@ -0,0 +1,54 @@
+package alin.licenta.be.controllers;
+
+import alin.licenta.be.dto.AssignedValueDTO;
+import alin.licenta.be.services.AssignedValueService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/assignedValue")
+public class AssignedValueController {
+
+    AssignedValueService assignedValueService;
+
+    @Autowired
+    public AssignedValueController(AssignedValueService assignedValueService) {
+        this.assignedValueService = assignedValueService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Object> create(@RequestBody AssignedValueDTO assignedValueDTO) {
+        return new ResponseEntity<>(assignedValueService.create(assignedValueDTO), HttpStatus.OK);
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Object> findById(@PathVariable Integer id) {
+        return new ResponseEntity<>(assignedValueService.findDTOById(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<AssignedValueDTO>> findAll() {
+        return new ResponseEntity<>(assignedValueService.findAll(), HttpStatus.OK);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Object> update(@PathVariable Integer id, @RequestBody AssignedValueDTO assignedValueDTO) {
+        return new ResponseEntity<>(assignedValueService.update(id, assignedValueDTO), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Integer> deleteById(@PathVariable Integer id) {
+        assignedValueService.deleteById(id);
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/deleteAll")
+    public ResponseEntity<Object> deleteAll() {
+        assignedValueService.deleteAll();
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
